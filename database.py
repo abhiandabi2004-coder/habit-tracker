c84b6f6e44a15378493c505314edf0f34f072d58
abhiandabi2004-coder <abhiandabi2004@gmail.com>
2026-02-19 02:14:09 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
 import sqlite3
 
+DB_NAME = "habits.db"
+
 def connect_db():
-    return sqlite3.connect("database/habits.db", check_same_thread=False)
+    return sqlite3.connect(DB_NAME, check_same_thread=False)
 
 def create_tables():
     conn = connect_db()
     cursor = conn.cursor()
 
+    # USERS TABLE
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users(
+    CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         age INTEGER,
@@ -19,20 +22,24 @@ def create_tables():
     )
     """)
 
+    # HABITS TABLE
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS habits(
+    CREATE TABLE IF NOT EXISTS habits (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
         habit_name TEXT,
-        target_days INTEGER
+        target_days INTEGER,
+        FOREIGN KEY (user_id) REFERENCES users(id)
     )
     """)
 
+    # HABIT LOGS TABLE
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS habit_logs(
+    CREATE TABLE IF NOT EXISTS habit_logs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         habit_id INTEGER,
-        date TEXT
+        date TEXT,
+        FOREIGN KEY (habit_id) REFERENCES habits(id)
     )
     """)
 
