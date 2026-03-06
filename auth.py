1588b7df631c928bce182a47b82ae18a444185fe
abhiandabi2004-coder <abhiandabi2004@gmail.com>
2026-03-06 12:29:51 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,27 +1,27 @@
 from database import connect_db
-import sqlite3
 
 def register_user(name, age, gender, occupation, username, password):
-    try:
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("""
-            INSERT INTO users(name, age, gender, occupation, username, password)
-            VALUES (?, ?, ?, ?, ?, ?)
-        """, (name, age, gender, occupation, username, password))
-        conn.commit()
-        conn.close()
-        return "success"
-    except sqlite3.IntegrityError:
-        return "duplicate"
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO users(name, age, gender, occupation, username, password)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """, (name, age, gender, occupation, username, password))
+
+    conn.commit()
+    conn.close()
+
 
 def login_user(username, password):
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute(
-        "SELECT * FROM users WHERE username=? AND password=?",
-        (username, password),
-    )
+
+    cursor.execute("""
+    SELECT * FROM users WHERE username=%s AND password=%s
+    """, (username, password))
+
     user = cursor.fetchone()
+
     conn.close()
     return user
