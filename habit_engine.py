2094b34e99caecd9f29dba6090dc9477c98deb90
abhiandabi2004-coder <abhiandabi2004@gmail.com>
2026-03-06 12:30:33 +0530

    Update habit_engine.py

--- habit_engine.py
+++ habit_engine.py
@@ -1,93 +1,42 @@
-from datetime import datetime, timedelta
 from database import connect_db
+from datetime import date
 
 
 def add_habit(user_id, habit_name, target_days):
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO habits(user_id, habit_name, target_days) VALUES (?, ?, ?)",
-        (user_id, habit_name, target_days),
-    )
+
+    cursor.execute("""
+    INSERT INTO habits(user_id, habit_name, target_days)
+    VALUES (%s, %s, %s)
+    """, (user_id, habit_name, target_days))
+
     conn.commit()
     conn.close()
 
 
-def get_user_habits(user_id):
+def get_habits(user_id):
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM habits WHERE user_id=?", (user_id,))
+
+    cursor.execute("""
+    SELECT * FROM habits WHERE user_id=%s
+    """, (user_id,))
+
     habits = cursor.fetchall()
     conn.close()
+
     return habits
 
 
-def update_habit(habit_id, new_name, new_target):
+def mark_habit(habit_id):
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute(
-        "UPDATE habits SET habit_name=?, target_days=? WHERE id=?",
-        (new_name, new_target, habit_id),
-    )
+
+    cursor.execute("""
+    INSERT INTO habit_logs(habit_id, date, value)
+    VALUES (%s, %s, %s)
+    """, (habit_id, date.today(), 1))
+
     conn.commit()
     conn.close()
-
-
-def delete_habit(habit_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM habits WHERE id=?", (habit_id,))
-    conn.commit()
-    conn.close()
-
-
-def log_habit(habit_id, value):
-    conn = connect_db()
-    cursor = conn.cursor()
-
-    today = datetime.now().date().isoformat()
-
-    cursor.execute(
-        "SELECT 1 FROM habit_logs WHERE habit_id=? AND date=?",
-        (habit_id, today),
-    )
-
-    if not cursor.fetchone():
-        cursor.execute(
-            "INSERT INTO habit_logs(habit_id, date, value) VALUES (?, ?, ?)",
-            (habit_id, today, value),
-        )
-        conn.commit()
-
-    conn.close()
-
-
-def calculate_streak(habit_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "SELECT date FROM habit_logs WHERE habit_id=? ORDER BY date DESC",
-        (habit_id,),
-    )
-
-    dates = cursor.fetchall()
-    conn.close()
-
-    if not dates:
-        return 0
-
-    dates = [datetime.strptime(d[0], "%Y-%m-%d").date() for d in dates]
-
-    today = datetime.now().date()
-    streak = 0
-    current_day = today
-
-    for d in dates:
-        if d == current_day:
-            streak += 1
-            current_day -= timedelta(days=1)
-        else:
-            break
-
-    return streak
